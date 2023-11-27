0be65092ceb6d2abdf3e5cadc887a9dedf6e6fbd
tanu <dastanushri402@gmail.com>
2023-11-27 19:51:00 +0600

    do bottom fixed

--- src/Components/Shared/Footer/Footer.jsx
+++ src/Components/Shared/Footer/Footer.jsx
@@ -1,36 +1,44 @@
+// Footer.js
+
 import React from "react";
 import { FaFacebook, FaLinkedin, FaYoutube } from "react-icons/fa";
 import "./Footer.css";
 
 const Footer = () => {
   const date = new Date().getFullYear();
+
   return (
     <div className="footer">
-      <p className="copy">
-        &copy; {date} - All rights reserved by Tanushri Das
-      </p>
-      <div className="social-links">
-        <a className="social-link"
-          href="https://www.linkedin.com/in/tanushri-das-06a520194/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <FaLinkedin className="linkedin" />
-        </a>
-        <a className="social-link"
-          href="https://www.facebook.com/tanushri.das01?mibextid=ZbWKwL"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <FaFacebook className="facebook" />
-        </a>
-        <a 
-          href="https://www.youtube.com/watch?v=ZvggB9FT4gM&ab_channel=MuseumofFineArts%2CBoston"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <FaYoutube className="youtube" />
-        </a>
+      <div className="footer-div">
+        <p className="copy">
+          &copy; {date} - All rights reserved by Tanushri Das
+        </p>
+        <div className="social-links">
+          <a
+            className="social-link"
+            href="https://www.linkedin.com/in/tanushri-das-06a520194/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaLinkedin className="linkedin" />
+          </a>
+          <a
+            className="social-link"
+            href="https://www.facebook.com/tanushri.das01?mibextid=ZbWKwL"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaFacebook className="facebook" />
+          </a>
+          <a
+            className="social-link"
+            href="https://www.youtube.com/watch?v=ZvggB9FT4gM&ab_channel=MuseumofFineArts%2CBoston"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaYoutube className="youtube" />
+          </a>
+        </div>
       </div>
     </div>
   );
